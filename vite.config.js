767fe322835c7caf271f6f664ddcdb8da5821097
pal94star <palashsutradhartrader@gmail.com>
2025-11-07 00:47:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  base: "/market-pathshala/"
+  base: "/Equity-anchor/"   // 👈 must match your repo name exactly
 });
+
